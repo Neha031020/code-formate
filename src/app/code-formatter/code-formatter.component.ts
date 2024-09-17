ca3ca4f73794422c862a2f7ad3f4fa43b9a57b6e
Neha031020 <nehaadlak0310@gmail.com>
2024-09-17 11:39:00 +0530

    code formate

--- src/app/code-formatter/code-formatter.component.ts
+++ src/app/code-formatter/code-formatter.component.ts
@@ -10,49 +10,45 @@ export class CodeFormatterComponent {
 
   codeInput!: any;
   beautify!: any;
-  language  = ''
+  language = ''
 
-  formateCode(){
-    console.log("this.codeInput",this.codeInput);
-    
+  /**
+   * formate document
+   *
+   * @memberof CodeFormatterComponent
+   */
+  formateCode() {
     try {
-      const code = this.codeInput.trim(); // trim the code to remove whitespace
-    let formattedCode = '';
-
-    // detect the language based on the code
-    if (code.startsWith('<') && code.endsWith('>')) {
-      this.language = 'html';
-      formattedCode = beautify.html(code, {
-        indent_size: 2,
-        wrap_line_length: 80
-      });
-    } else if (code.startsWith('/*') && code.endsWith('*/')) {
-      this.language = 'css';
-      formattedCode = beautify.css(code, {
-        indent_size: 2
-      });
-    } else if (code.startsWith('{') && code.endsWith('}')) {
-      this.language = 'json';
-      formattedCode = beautify.js(code, {
-        indent_size: 2,
-        space_in_empty_paren: true,
-        unescape_strings: true
-      });
-    } else {
-      this.language = 'js';
-      formattedCode = beautify.js(code, {
-        indent_size: 2,
-        space_in_empty_paren: true
-      });
-    }
-
-    this.codeInput = formattedCode;
-      
+      const code = this.codeInput.trim();
+      let formattedCode = '';
+      if (code.startsWith('<') && code.endsWith('>')) {
+        this.language = 'html';
+        formattedCode = beautify.html(code, {
+          indent_size: 2,
+          wrap_line_length: 80
+        });
+      } else if (code.startsWith('/*') && code.endsWith('*/')) {
+        this.language = 'css';
+        formattedCode = beautify.css(code, {
+          indent_size: 2
+        });
+      } else if (code.startsWith('{') && code.endsWith('}')) {
+        this.language = 'json';
+        formattedCode = beautify.js(code, {
+          indent_size: 2,
+          space_in_empty_paren: true,
+          unescape_strings: true
+        });
+      } else {
+        this.language = 'js';
+        formattedCode = beautify.js(code, {
+          indent_size: 2,
+          space_in_empty_paren: true
+        });
+      }
+      this.codeInput = formattedCode;
     } catch (error) {
       alert('Error formatting code. Please check your syntax.');
     }
-    console.log(" this.codeInput ", this.codeInput );
-    
   }
-
 }
